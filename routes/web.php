0978657d967f22c98c4aeb091a5354f3231ee8fd
Opicksss <upek123slopeng@gmail.com>
2025-12-13 13:28:06 +0700

    ke4

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,9 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HistoryController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\petani\PetaniController;
-use App\Http\Controllers\ahli\hama\KriteriaHamaController;
-use App\Http\Controllers\ahli\hama\alternatifHamaController;
-use App\Http\Controllers\ahli\hama\SubKriteriaHamaController;
-use App\Http\Controllers\petani\PetaniPenyakitController as PetaniPetaniPenyakitController;
-
+use App\Http\Controllers\Ahp\AlternatifController;
+use App\Http\Controllers\Ahp\KriteriaController;
+use App\Http\Controllers\Ahp\SubKriteriaController;
 
 Route::middleware(['guest'])->group(function () {
     Route::get('/', [AuthController::class, 'login'])->name('login');
@@ -27,32 +25,28 @@ Route::middleware(['auth'])->group(function () {
     Route::middleware(['userAkses:admin'])->group(function () {
         Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-        Route::get('/kriteria', [KriteriaHamaController::class, 'index'])->name('kriteria.index');
-        Route::post('/kriteria', [KriteriaHamaController::class, 'store'])->name('kriteria.post');
-        Route::put('/kriteria{id}', [KriteriaHamaController::class, 'update'])->name('kriteria.put');
-        Route::delete('/kriteria{id}', [KriteriaHamaController::class, 'delete'])->name('kriteria.delete');
-        Route::get('/kriteria-Matriks', [KriteriaHamaController::class, 'matriks'])->name('kriteria.matriks');
-        Route::post('/kriteria-Matriks/store', [KriteriaHamaController::class, 'storeMatriks'])->name('kriteria.matriks.store');
+        Route::get('/kriteria', [KriteriaController::class, 'index'])->name('kriteria.index');
+        Route::post('/kriteria', [KriteriaController::class, 'store'])->name('kriteria.post');
+        Route::put('/kriteria{id}', [KriteriaController::class, 'update'])->name('kriteria.put');
+        Route::delete('/kriteria{id}', [KriteriaController::class, 'delete'])->name('kriteria.delete');
+        Route::get('/kriteria-Matriks', [KriteriaController::class, 'matriks'])->name('kriteria.matriks');
+        Route::post('/kriteria-Matriks/store', [KriteriaController::class, 'storeMatriks'])->name('kriteria.matriks.store');
 
-        Route::get('/subKriteria{id}', [SubKriteriaHamaController::class, 'index'])->name('subKriteria.index');
-        Route::post('/subKriteria', [SubKriteriaHamaController::class, 'post'])->name('subKriteria.post');
-        Route::put('/subKriteria{id}', [SubKriteriaHamaController::class, 'put'])->name('subKriteria.put');
-        Route::delete('/subKriteria{id}', [SubKriteriaHamaController::class, 'delete'])->name('subKriteria.delete');
-        Route::get('/subKriteria{id}/matriks/go', [SubKriteriaHamaController::class, 'matriks'])->name('matriks');
-        Route::post('/subKriteria{id}/matriks/store/go', [SubKriteriaHamaController::class, 'postMatriks'])->name('matriks.post');
+        Route::get('/subKriteria{id}', [SubKriteriaController::class, 'index'])->name('subKriteria.index');
+        Route::post('/subKriteria', [SubKriteriaController::class, 'post'])->name('subKriteria.post');
+        Route::put('/subKriteria{id}', [SubKriteriaController::class, 'put'])->name('subKriteria.put');
+        Route::delete('/subKriteria{id}', [SubKriteriaController::class, 'delete'])->name('subKriteria.delete');
+        Route::get('/subKriteria{id}/matriks/go', [SubKriteriaController::class, 'matriks'])->name('matriks');
+        Route::post('/subKriteria{id}/matriks/store/go', [SubKriteriaController::class, 'postMatriks'])->name('matriks.post');
 
-        Route::get('/alternatif', [alternatifHamaController::class, 'index'])->name('alternatif.index');
-        Route::post('/alternatif', [alternatifHamaController::class, 'store'])->name('alternatif.post');
-        Route::put('/alternatif{id}', [alternatifHamaController::class, 'update'])->name('alternatif.put');
-        Route::delete('/alternatif{id}', [alternatifHamaController::class, 'delete'])->name('alternatif.delete');
-        Route::get('/penilaian-alternatif', [AlternatifHamaController::class, 'tampilPenilaianAlternatif'])->name('alternatif.penilaian.form');
-        Route::post('/penilaian-alternatif', [AlternatifHamaController::class, 'simpanPenilaian'])->name('alternatif.penilaian.simpan');
+        Route::get('/alternatif', [AlternatifController::class, 'index'])->name('alternatif.index');
+        Route::post('/alternatif', [AlternatifController::class, 'store'])->name('alternatif.post');
+        Route::put('/alternatif{id}', [AlternatifController::class, 'update'])->name('alternatif.put');
+        Route::delete('/alternatif{id}', [AlternatifController::class, 'delete'])->name('alternatif.delete');
+        Route::get('/penilaian-alternatif', [AlternatifController::class, 'tampilPenilaianAlternatif'])->name('alternatif.penilaian.form');
+        Route::post('/penilaian-alternatif', [AlternatifController::class, 'simpanPenilaian'])->name('alternatif.penilaian.simpan');
 
-        Route::get('/', [AdminController::class, 'admin'])->name('admin.admin');
-        Route::get('/user', [AdminController::class, 'user'])->name('admin.user');
-        Route::post('/', [AdminController::class, 'store'])->name('admin.store');
-        Route::put('/{id}', [AdminController::class, 'update'])->name('admin.update');
-        Route::delete('/{id}', [AdminController::class, 'destroy'])->name('admin.destroy');
+
 
         // Buatkan route management account
         Route::get('/management-account', [AdminController::class, 'index'])->name('users.index');
